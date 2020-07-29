6d2428436feb8e4c8b4cd4186fb28ebb943fa32e
Olivia Ma <olivia.ma@myxing.net>
2020-07-29 15:18:02 -0400

    done

--- crossword.py
+++ crossword.py
@@ -8,6 +8,8 @@ from pathlib import Path
 import random
 import json
 import string
+from collections import defaultdict
+import pickle
 
 BLOCKCHAR = '#'
 OPENCHAR = '-'
@@ -15,10 +17,10 @@ PROTECTEDCHAR = '~'
 
 
 class Crossword:
-    def __init__(self, vocab_file, width=8, height=8):
+    def __init__(self, vocab_file=r'vocab.pkl', width=8, height=8):
         """
         :param vocab_file: string
-            name of file with all possible words and their definitions
+            pickle file with words and meanings
         :param width: int
             width of board
         :param height: int
@@ -28,16 +30,8 @@ class Crossword:
         self.height = height
         self.board = '-' * (width * height)
         self.blockct = int(width * height / 6)
-        self.used_words = dict()
-        self.vocab = dict()
-        path = Path(vocab_file)
-        with open(path) as opened_file:
-            dictionary = json.load(opened_file)
-        for word in dictionary:
-            self.vocab[word.upper()] = [sense_num[1] for sense_num in dictionary[word][0]]
-        self.vocab['PETAR'] = ['the one true king']
-        self.vocab['COGWORKS'] = ['the best bwsi course ;)']
-        self.vocab['DOG'].append('a good boi')
+        self.vocab = defaultdict(list)
+        self.load_vocab(vocab_file)
 
         # make board
         empty_pos = [i for i in range(len(self.board))]
@@ -48,7 +42,16 @@ class Crossword:
         self.patterns = dict()
         self.letter_count = {letter: 0 for letter in string.ascii_uppercase}
         self.make_patterns()
-        self.down, self.across = self.find_word_pos()
+        self.down, self.across = self.find_word_pos(self.board)
+        self.solved_board = self.solve_board(self.board, set(), self.down, self.across)
+
+        # show puzzle
+        self.get_game()
+
+    def load_vocab(self, path):
+        path = Path(path)
+        with open(path, mode='rb') as openedfile:
+            self.vocab = pickle.load(openedfile)
 
     def place_blocks(self, board, curr_blockct, empty_pos):
         """
@@ -181,11 +184,17 @@ class Crossword:
     def transpose(self, xw, newWidth):
         return ''.join([xw[col::newWidth] for col in range(newWidth)])
 
-    def display_board(self):
+    def display_board(self, board):
         for i in range(self.height):
             for j in range(self.width):
-                print(self.board[i * self.width + j], end=" ")
+                print(board[i * self.width + j], end=" ")
             print()
+
+    def display_board_in_file(self, board, file):
+        for i in range(self.height):
+            for j in range(self.width):
+                print(board[i * self.width + j], end=" ", file=file)
+            print('\n', file=file)
 
     def make_patterns(self):
         for word in self.vocab:
@@ -207,25 +216,25 @@ class Crossword:
                     self.patterns[new_key] = words
                 self.letter_count[word[pos]] += 1
 
-    def find_word_pos(self):
+    def find_word_pos(self, board):
         down = dict()
         across = dict()
-        for pos in range(len(self.board)):
-            if self.board[pos] != '#' and (pos % self.width == 0 or self.board[pos - 1] == '#'):
-                pattern, a = self.board[pos], pos + 1
-                while a < pos + self.width - pos % self.width and self.board[a] != '#':
-                    pattern += self.board[a]
+        for pos in range(len(board)):
+            if board[pos] != '#' and (pos % self.width == 0 or board[pos - 1] == '#'):
+                pattern, a = board[pos], pos + 1
+                while a < pos + self.width - pos % self.width and board[a] != '#':
+                    pattern += board[a]
                     a += 1
                 across[pos] = pattern
-            if self.board[pos] != '#' and (pos < self.width or self.board[pos - self.width] == '#'):
-                pattern, a = self.board[pos], pos + self.width
-                while a < len(self.board) and self.board[a] != '#':
-                    pattern += self.board[a]
+            if board[pos] != '#' and (pos < self.width or board[pos - self.width] == '#'):
+                pattern, a = board[pos], pos + self.width
+                while a < len(board) and board[a] != '#':
+                    pattern += board[a]
                     a += self.width
                 down[pos] = pattern
         return down, across
 
-    def solve_board(self, board):
+    def solve_board(self, board, used, down, across):
         """
         fill board in with words from vocab, save their positions, direction, and definition
         :return:
@@ -236,19 +245,124 @@ class Crossword:
         # find words that can fit that pattern, choose the most likely to be used (common word? common letter?)
         # for every possible word, place it in the board and recur
         if '-' not in board:
-            d_words, a_words = self.find_word_pos()
-            for word in d_words.values():
+            for word in down.values():
                 if word not in self.vocab:
                     return None
-            for word in a_words.values():
+            for word in across.values():
                 if word not in self.vocab:
                     return None
             return board
-        pass
+        pattern, index, possible_words = self.most_constrained_pattern(down, across)
+        possible_words = list(set(possible_words) - set(used))
+        possible_words.sort(key=self.heuristic, reverse=True)
+        for word in possible_words:
+            new_board = self.add_word(board, word, index)
+            new_used_words = used.copy()
+            new_used_words.add(word)
+            new_down, new_across = self.find_word_pos(new_board)
+            if index[1] == 'D':
+                words = self.new_words(across, new_across)
+            else:
+                words = self.new_words(down, new_down)
+            bad_board = False
+            for pos in words:
+                pat = words[pos]
+                if '-' not in pat:
+                    if pat in used or pat not in self.vocab:
+                        bad_board = True
+                        break
+                    else:
+                        new_used_words.add(pat)
+                else:
+                    if index[1] == "A":
+                        word_list = self.get_possible_words(pat, (pos % self.width + index[0] - pos) // self.width)
+                    else:
+                        word_list = self.get_possible_words(pat, index[0] + (pos // self.width - index[0] // self.width) * self.width - pos)
+                    word_list = list(set(word_list) - set(new_used_words))
+                    if len(word_list) == 0:
+                        bad_board = True
+                        break
+            if bad_board:
+                continue
+            else:
+                newBoard = self.solve_board(new_board, new_used_words, new_down, new_across)
+                if newBoard is not None:
+                    return newBoard
+        return None
+
+    def heuristic(self, word):
+        val = 0
+        for letter in word:
+            if letter in self.letter_count:
+                val += self.letter_count[letter]
+        return val
+
+    def most_constrained_pattern(self, down, across):
+        words = None
+        pattern, index = '', (-1, 'x')
+        for pos in down:
+            pat = down[pos]
+            if pat.count('-') != 0:
+                possible_words = self.get_possible_words(pat)
+                if words is None or len(possible_words) <= len(words):
+                    words = possible_words
+                    pattern, index = pat, (pos, "D")
+        for pos in across:
+            pat = across[pos]
+            if pat.count('-') != 0:
+                possible_words = self.get_possible_words(pat)
+                if words is None or len(possible_words) <= len(words):
+                    words = possible_words
+                    pattern, index = pat, (pos, "A")
+        return pattern, index, words
+
+    def get_possible_words(self, pattern, new_pos=-1):
+        if pattern in self.patterns or new_pos == -1:
+            return self.patterns[pattern]
+        elif len(pattern.strip('-')) == 1:
+            return set()
+        else:
+            new_pat = '-' * new_pos + pattern[new_pos] + '-' * (len(pattern) - new_pos - 1)
+            if new_pat in self.patterns:
+                new_words = self.patterns[new_pat]
+            else:
+                new_words = set()
+            old_words = self.patterns[pattern[:new_pos] + '-' + pattern[new_pos + 1:]]
+            possible_words = new_words & old_words
+            self.patterns[pattern] = possible_words
+            return possible_words
+
+    def add_word(self, board, word, index):
+        if index[1] == 'A':
+            start = index[0]
+            board = board[:start] + word + board[start + len(word):]
+        else:
+            start = index[0]
+            for i in range(len(word)):
+                pos = start + i * self.width
+                board = board[:pos] + word[i] + board[pos + 1:]
+        return board
+
+    def new_words(self, old, new):
+        words = dict()
+        for key in old:
+            if new[key] != old[key]:
+                words[key] = new[key]
+        return words
 
     def get_game(self):
         """
         makes a file with the clean generated board and words/clues
         :return:
         """
-        pass
+        f = open('crossword.txt', 'w')
+        self.display_board_in_file(self.board, f)
+        print("DOWN", file=f)
+        self.down, self.across = self.find_word_pos(self.solved_board)
+        for pos in self.down:
+            print(pos, ': ', random.choice(self.vocab[self.down[pos]]), end='\n', file=f)
+        print("ACROSS", file=f)
+        for pos in self.across:
+            print(pos, ': ', random.choice(self.vocab[self.across[pos]]), end='\n', file=f)
+        print("Game ready")
+        f.close()
