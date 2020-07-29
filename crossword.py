bcf38d04640e4a59547a2be46d691b28cf4db04f
Olivia Ma <olivia.ma@myxing.net>
2020-07-29 00:11:22 -0400

    setting up to solve board

--- crossword.py
+++ crossword.py
@@ -1,10 +1,13 @@
 # crossword class
 # crossword object creates an empty board with word bank and clues
 
-# nee to find a file w words and their definition -> (word: definition)
+# nee to find a file w words and their definition -> (word: [definitions])
+# how to open all the json files in a folder and read all of them?
 import re
 from pathlib import Path
 import random
+import json
+import string
 
 BLOCKCHAR = '#'
 OPENCHAR = '-'
@@ -28,15 +31,24 @@ class Crossword:
         self.used_words = dict()
         self.vocab = dict()
         path = Path(vocab_file)
-        with open(path, mode="r") as opened_file:
-            lines = opened_file.readlines()
-        for line in lines:
-            word, definition = line.split(': ')
-            self.vocab[word] = definition
+        with open(path) as opened_file:
+            dictionary = json.load(opened_file)
+        for word in dictionary:
+            self.vocab[word.upper()] = [sense_num[1] for sense_num in dictionary[word][0]]
+        self.vocab['PETAR'] = ['the one true king']
+        self.vocab['COGWORKS'] = ['the best bwsi course ;)']
+        self.vocab['DOG'].append('a good boi')
 
+        # make board
         empty_pos = [i for i in range(len(self.board))]
         self.board, num = self.place_blocks(self.board, 0, empty_pos)
         self.clean_protected()
+
+        # solve puzzle
+        self.patterns = dict()
+        self.letter_count = {letter: 0 for letter in string.ascii_uppercase}
+        self.make_patterns()
+        self.down, self.across = self.find_word_pos()
 
     def place_blocks(self, board, curr_blockct, empty_pos):
         """
@@ -62,7 +74,7 @@ class Crossword:
                 curr_blockct = board.count(BLOCKCHAR)
             else:
                 bordered = self.add_border(new_board)
-                c = self.area_fill(bordered, self.width + 2, bordered.find(OPENCHAR), set())
+                c = self.check_connected(bordered, self.width + 2, bordered.find(OPENCHAR), set())
                 if c != len(new_board) - new_board.count(BLOCKCHAR):
                     board = board[0:picked_pos] + OPENCHAR + board[picked_pos + 1:]
                     curr_blockct = board.count(BLOCKCHAR)
@@ -71,7 +83,7 @@ class Crossword:
                     final_board, curr_blockct = self.place_blocks(new_board, curr_blockct, temp_pos)
                     if curr_blockct == self.blockct:
                         bordered = self.add_border(final_board)
-                        connected = self.area_fill(bordered, self.width + 2, bordered.find(OPENCHAR), set())
+                        connected = self.check_connected(bordered, self.width + 2, bordered.find(OPENCHAR), set())
                         if connected == len(final_board) - final_board.count(BLOCKCHAR):
                             return final_board, curr_blockct
                         else:
@@ -120,16 +132,16 @@ class Crossword:
         new_board = re.sub("[A-Z]", "~", new_board)
         return self.combine(board, new_board)
 
-    def area_fill(self, board, width, index, visited):
+    def check_connected(self, board, width, index, visited):
         if index < 0 or index >= len(board):
             return 0
         if index in visited or board[index] == BLOCKCHAR:
             return 0
         visited.add(index)
-        return 1 + self.area_fill(board, width, index - 1, visited) + \
-               self.area_fill(board, width, index - width, visited) + \
-               self.area_fill(board, width, index + 1, visited) + \
-               self.area_fill(board, width, index + width, visited)
+        return 1 + self.check_connected(board, width, index - 1, visited) + \
+               self.check_connected(board, width, index - width, visited) + \
+               self.check_connected(board, width, index + 1, visited) + \
+               self.check_connected(board, width, index + width, visited)
 
     def add_border(self, board):  # adds border to board
         xw = BLOCKCHAR * (self.width + 3)
@@ -175,11 +187,63 @@ class Crossword:
                 print(self.board[i * self.width + j], end=" ")
             print()
 
-    def solve_board(self):
+    def make_patterns(self):
+        for word in self.vocab:
+            word = word.upper()
+            key = '-' * len(word)
+            if key not in self.patterns:
+                self.patterns[key] = {word}
+            else:
+                words = self.patterns[key]
+                words.add(word)
+                self.patterns[key] = words
+            for pos in range(len(word)):
+                new_key = key[:pos] + word[pos] + key[pos + 1:]
+                if new_key not in self.patterns:
+                    self.patterns[new_key] = {word}
+                else:
+                    words = self.patterns[new_key]
+                    words.add(word)
+                    self.patterns[new_key] = words
+                self.letter_count[word[pos]] += 1
+
+    def find_word_pos(self):
+        down = dict()
+        across = dict()
+        for pos in range(len(self.board)):
+            if self.board[pos] != '#' and (pos % self.width == 0 or self.board[pos - 1] == '#'):
+                pattern, a = self.board[pos], pos + 1
+                while a < pos + self.width - pos % self.width and self.board[a] != '#':
+                    pattern += self.board[a]
+                    a += 1
+                across[pos] = pattern
+            if self.board[pos] != '#' and (pos < self.width or self.board[pos - self.width] == '#'):
+                pattern, a = self.board[pos], pos + self.width
+                while a < len(self.board) and self.board[a] != '#':
+                    pattern += self.board[a]
+                    a += self.width
+                down[pos] = pattern
+        return down, across
+
+    def solve_board(self, board):
         """
         fill board in with words from vocab, save their positions, direction, and definition
         :return:
         """
+        # if board is full, check that all words in board are real words (ie in the vocab)
+        # if a word is not a real word, return none; if all words are real, return board
+        # find position w/ pattern easiest to fill in (has the fewest possibilities, or already partially filled)
+        # find words that can fit that pattern, choose the most likely to be used (common word? common letter?)
+        # for every possible word, place it in the board and recur
+        if '-' not in board:
+            d_words, a_words = self.find_word_pos()
+            for word in d_words.values():
+                if word not in self.vocab:
+                    return None
+            for word in a_words.values():
+                if word not in self.vocab:
+                    return None
+            return board
         pass
 
     def get_game(self):
